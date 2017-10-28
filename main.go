290faa8de713f432da048b47daed50a57266c1fe
Christian <cbt_97@hotmail.com>
2017-10-28 13:41:44 +0200

    test

--- main.go
+++ main.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", handler)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error(), " Panic or something")
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+func handler(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, world")
+
+	timer := time.NewTimer(time.Second * 10).C
+
+	timerFinished := make(chan bool)
+	go func() {
+		time.Sleep(time.Second * 2)
+		timerFinished <- true
+	}()
+
+	for {
+		select {
+		case <-timer:
+			fmt.Println("Timer expired")
+		case <-timerFinished:
+			fmt.Println("Done")
+			return
+		}
+	}
+
 }
