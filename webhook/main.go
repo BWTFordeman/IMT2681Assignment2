ff81fa43e6cb2a8284acda057d4b512688dedfc9
Christian <cbt_97@hotmail.com>
2017-10-30 17:44:45 +0100

    Put main in directory

--- webhook/main.go
+++ webhook/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-//Postload datatype I get from fixer.io
+//Postload data retrieved from adding webhook
 type Postload struct {
 	WebhookURL      string  `json:"webhookURL"`
 	BaseCurrency    string  `json:"baseCurrency"`
@@ -17,20 +17,24 @@ type Postload struct {
 	MaxTriggerValue float32 `json:"maxTriggerValue"`
 }
 
-/*
-type Payload struct {
-	WebhookURL   string `json:"url"`
-	BaseCurrency string `json:"baseCurrency"`
-	Date         int    `json:"date"`
-	Rates        struct {
-		currency map[string]int
-	} `json:"rates"`
-}*/
+//Webhook retrieves data from the webhook collection:
+type Webhook struct {
+	WebhookURL      string  `json:"webhookURL"`
+	BaseCurrency    string  `json:"baseCurrency"`
+	TargetCurrency  string  `json:"targetCurrency"`
+	MinTriggerValue float32 `json:"minTriggerValue"`
+	MaxTriggerValue float32 `json:"maxTriggerValue"`
+	CurrentRate     float32 `json:"currentRate"`
+}
 
-//Make another one for requesting data from fixer.io
-/*
-using the data:			api.fixer.io/latest?base=EUR;symbols=NOK
-{"base": "EUR", "date":"2017-10-27", "rates":{"NOK":9.5348}}*/
+//Fixer retrieves latest data from fixer.io							api.fixer.io/latest?base=EUR;symbols=NOK
+type Fixer struct {
+	BaseCurrency string `json:"baseCurrency"`
+	Date         string `json:"date"`
+	Rates        struct {
+		string float32
+	} `json:"rates"`
+}
 
 //invokeWebhook sends messages through webhooks created in the system
 //Must take away lang when database is added, and search through database for names instead.
@@ -47,12 +51,12 @@ func invokeWebhook(w http.ResponseWriter, lang [32]string) {
 	if res.StatusCode == 200 || res.StatusCode == 204 {
 		fmt.Fprintln(w, "statuscode: ", res.StatusCode)
 	} else {
-		fmt.Fprintln(w, "Wrong status: ", res.StatusCode)
+		fmt.Fprintln(w, "Wrong status: ", res.StatusCode, http.StatusText(res.StatusCode))
 	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -60,7 +64,7 @@ func main() {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request) {
 	lang := [...]string{"EUR", "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW",
 		"MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"}
 
@@ -84,8 +88,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		//Create row in databse if valid:
 		if err != nil || base != true || target != true {
 			http.Error(w, "Invalid post value", http.StatusBadRequest)
-		} else {
-			//put in database here.
+		} else { //Create data in database:
+
 			fmt.Fprintln(w, "Id for your webhook: ") // add an id generated here.
 		}
 
@@ -95,7 +99,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Webhook:		//Needs to be in another handler
-	invokeWebhook(w, lang)
+	//invokeWebhook(w, lang)
 
 }
 
