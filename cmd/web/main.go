dfc128ccb9a89371715ff3c194af88d037b588b3
Christian <cbt_97@hotmail.com>
2017-11-01 22:06:36 +0100

    gorilla mux

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -65,7 +65,7 @@ func getWebhooks(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		http.Error(w, "Object doesn't exist", http.StatusBadRequest)
 	} else {
-		fmt.Fprintln(w, "here it is:", d)
+		fmt.Fprintln(w, "here it is:", d.ID, d.BaseCurrency)
 	}
 }
 
