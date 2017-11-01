ef006586cdb2ee9a80bb7cebbe30f1b2ae64994b
Christian <cbt_97@hotmail.com>
2017-11-01 12:57:01 +0100

    almost done with clock

--- cmd/clock/main.go
+++ cmd/clock/main.go
@@ -9,7 +9,19 @@ import (
 	"time"
 
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+//Webhook retrieves data from the webhook collection:
+type Webhook struct {
+	ID              bson.ObjectId `json:"_id" bson:"_id"`
+	WebhookURL      string        `json:"webhookURL"`
+	BaseCurrency    string        `json:"baseCurrency"`
+	TargetCurrency  string        `json:"targetCurrency"`
+	MinTriggerValue float32       `json:"minTriggerValue"`
+	MaxTriggerValue float32       `json:"maxTriggerValue"`
+	CurrentRate     float32       `json:"currentRate"`
+}
 
 //Fixer retrieves latest data from fixer.io
 type Fixer struct {
@@ -61,38 +73,74 @@ func getFixerData() {
 	if err != nil {
 		fmt.Print("3", err.Error())
 	}
-
-	fmt.Println(f.BaseCurrency, f.Date, f.Rates)
-	//k := time.Now().AddDate(0, 0, -3)
+	k := time.Now().AddDate(0, 0, -3)
 
 	//Connect to database:
 	USER := os.Getenv("DB_USER")
 	PASSWORD := os.Getenv("DB_PASSWORD")
-	//DBNAME := os.Getenv("DB_NAME")
+	DBNAME := os.Getenv("DB_NAME")
 	tempstring := ("mongodb://" + USER + ":" + PASSWORD + "@ds241055.mlab.com:41055/imt2681")
-
 	session, err := mgo.Dial(tempstring)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error connecting to database", err.Error())
 	}
 	defer session.Close()
-	//add part to databse:
+
+	//If date doesn't already exist - add part to database:
+	d := Fixer{}
+	err = session.DB(DBNAME).C("fixerdata").Find(bson.M{"date": f.Date}).One(&d)
+	if err != nil {
+		err = session.DB(DBNAME).C("fixerdata").Insert(bson.M{"baseCurrency": f.BaseCurrency, "date": f.Date, "rates": f.Rates})
+		if err != nil {
+			fmt.Println("Error in Insert()", err.Error())
+		}
+	} else {
+		fmt.Println("fixerdata - Found object in database")
+	}
 	//if date of object is less than k then remove from database
+	err = session.DB(DBNAME).C("fixerdata").Remove(bson.M{"date": bson.M{"$lt": k.String()}})
+	if err != nil {
+		fmt.Println("fixerdata - No data older than 3 days")
+	}
+}
+
+//Check if data gotten from fixer exceedes someones threshold in webhook table.
+func updateWebhooks() {
+
+	//Connect to database:
+	USER := os.Getenv("DB_USER")
+	PASSWORD := os.Getenv("DB_PASSWORD")
+	DBNAME := os.Getenv("DB_NAME")
+	tempstring := ("mongodb://" + USER + ":" + PASSWORD + "@ds241055.mlab.com:41055/imt2681")
+	session, err := mgo.Dial(tempstring)
+	if err != nil {
+		fmt.Println("Error connecting to database", err.Error())
+	}
+	defer session.Close()
+
+	//Go through all webhooks
+	d := []Webhook{}
+	err = session.DB(DBNAME).C("webhooks").Find(nil).All(&d)
+	if err != nil {
+		//some error
+	} else {
+		fmt.Println("webhooks - found these:", d[0])
+	}
 }
 
 func main() {
 
 	getFixerData()
+	updateWebhooks()
 	start := time.Now()
 	for {
 		//time.Now().String()
 		delay := time.Minute * 20
 		elapsed := time.Now().Sub(start)
-		fmt.Println("\n", time.Now(), "\n", elapsed)
 		if elapsed > time.Hour*24 {
 			start = time.Now()
 			getFixerData()
-			//Check if data gotten from fixer exceedes someones threshold in webhook table.
+			updateWebhooks()
 		}
 		time.Sleep(delay)
 	}
