6171063e16ddfe8530b3c2ba7db54da00980eada
Christian <cbt_97@hotmail.com>
2017-11-03 15:41:32 +0100

    testing

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -32,3 +32,34 @@ func TestGetCurrentValue(t *testing.T) {
 		t.Errorf("Expected a value for NOO, got:%v", j)
 	}
 }
+
+
+
+
+
+/*package main
+
+import (
+	"testing"
+)
+
+//TODO...
+/*func TestGetFixerData(t *testing.T) {     //This could be another one for splitting up getFixerData
+
+}*/
+
+func TestGetFixerData(t *testing.T) {
+
+}
+
+func TestUpdateWebhooks(t *testing.T) {
+
+}
+func TestSendToWebhooks(t *testing.T) {
+
+}
+
+func TestInvokeWebhook(t *testing.T) {
+
+}
+*/
